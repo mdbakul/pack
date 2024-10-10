2d027aabf01d98a8d53f67121b3e298d408d918e
mdbakul <mdbakul51@gmail.com>
2024-10-10 11:52:57 +0600

    master

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -101,4 +101,46 @@ $(document).ready(function () {
         }
     })
 
+
+    // slick slider start here
+    // $(document).ready(function () {
+    //     $('.baner_slider').slick({
+
+    //     });
+    // });
+
+    $('.baner_slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
 });
