5f8fd66644840ee6c51526f708b6e593d316eff6
mdbakul <mdbakul51@gmail.com>
2024-10-12 12:11:14 +0600

    master

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -111,7 +111,7 @@ $(document).ready(function () {
 
     $('.baner_slider').slick({
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 5000,
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 4,
